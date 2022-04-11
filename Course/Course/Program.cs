054b3dd14563ddbea5fec0718185f221dee37e1e
Gabriel Lisboa <spgabriel08@hotmail.com>
2022-04-11 12:03:44 -0300

    Aula 124 Enumeracoes

---
+++ Course/Course/Program.cs
@@ -0,0 +1,28 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
