677226f22c8e7e215b4c70e8d4725c6e306174a1
Gabriel Lisboa <spgabriel08@hotmail.com>
2022-04-12 13:15:18 -0300

    Exercício Resolvido - Aula 130

---
+++ ExercicioProposto9/ExercicioProposto9/Entities/HourContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Course.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValurPerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valurPerHour, int hours)
+        {
+            Date = date;
+            ValurPerHour = valurPerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return ValurPerHour * Hours;
+        }
+    }
+}
