3595e15a380e613d554358b811356ac9b79cb09a
Gabriel Lisboa <spgabriel08@hotmail.com>
2022-04-26 17:16:33 -0300

    Base Finally - Aula 151

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,21 +1,22 @@
 ﻿using System;
+using System.IO;
 
-namespace Course {
-    class Program {
-        static void Main(string[] args) {
+public class ProcessFile {
+    public static void Main() {
 
-            try {
-                int n1 = int.Parse(Console.ReadLine());
-                int n2 = int.Parse(Console.ReadLine());
-
-                int result = n1 / n2;
-                Console.WriteLine(result);
-            }
-            catch (DivideByZeroException) {
-                Console.WriteLine("Divided by zero is not allowed");
-            }
-            catch (FormatException e) {
-                Console.WriteLine("Format error! " + e.Message);
+        FileStream fs = null;
+        try {
+            fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        catch (FileNotFoundException e) {
+            Console.WriteLine(e.Message);
+        }
+        finally {
+            if (fs != null) {
+                fs.Close();
             }
         }
     }
