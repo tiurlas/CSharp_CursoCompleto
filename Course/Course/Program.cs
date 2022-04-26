cb4812c5c24e1356f985807d1205e7b8fe251b61
Gabriel Lisboa <spgabriel08@hotmail.com>
2022-04-26 17:11:47 -0300

    Estrutura Try-Catch - Aula 150

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,50 +1,22 @@
-﻿using Course.Entities;
-using Course.Entities.Enums;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System;
 
-namespace Course
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Shape> list = new List<Shape>();
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
 
-            Console.Write("Enter the number of shapes: ");
-            int n = int.Parse(Console.ReadLine());
+            try {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i <= n; i++)
-            {
-                Console.WriteLine($"Shape #{i} data:");
-                Console.Write("Rectangle or Circle (r/c)? ");
-                char ch = char.Parse(Console.ReadLine());
-                Console.Write("Color (Black/Blue/Red): ");
-                Color color = (Color)Enum.Parse(typeof(Color), Console.ReadLine());
-                if (ch == 'r')
-                {
-                    Console.Write("Width: ");
-                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    Console.Write("Height: ");
-                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    list.Add(new Rectangle(width, height, color));
-                }
-                else
-                {
-                    Console.Write("Radius: ");
-                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    list.Add(new Circle(radius, color));
-                }                
+                int result = n1 / n2;
+                Console.WriteLine(result);
             }
-
-            Console.WriteLine();
-            Console.WriteLine("SHAPE AREAS:");
-            foreach (Shape shape in list)
-            {
-                Console.WriteLine(shape.Area().ToString("F2", CultureInfo.InvariantCulture));
+            catch (DivideByZeroException) {
+                Console.WriteLine("Divided by zero is not allowed");
             }
-
+            catch (FormatException e) {
+                Console.WriteLine("Format error! " + e.Message);
+            }
         }
     }
 }
